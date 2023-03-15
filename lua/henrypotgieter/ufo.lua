f49319d9b24b7d3836ee70d8de5f17bd2bbe8f0b
henry.potgieter <henry.potgieter@viscousbits.ca>
2023-03-15 00:49:44 -0400

    Change back to default foldlevel

--- lua/henrypotgieter/ufo.lua
+++ lua/henrypotgieter/ufo.lua
@@ -35,8 +35,8 @@ require("ufo").setup({
 })
 
 vim.o.foldcolumn = "1" -- '0' is not bad
-vim.o.foldlevel = 55  -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 1
+vim.o.foldlevel = 99  -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
 -- UFO Folding neatness
