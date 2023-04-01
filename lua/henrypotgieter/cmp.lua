474beddc7483dc8aa2aad61dd1f86f7435c265de
henry.potgieter <henry.potgieter@viscousbits.ca>
2023-04-01 02:37:56 -0400

    Rework how completion is handled

--- lua/henrypotgieter/cmp.lua
+++ lua/henrypotgieter/cmp.lua
@@ -64,12 +64,12 @@ cmp.setup({
 		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		--["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<C-Space>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 		--[[        -- Removed as it seems that supertab malfunctions at inopportune times
 		   [["<C-Tab>"] = cmp.mapping(function(fallback)
 		   [    if cmp.visible() then
